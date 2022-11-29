5858cdae6e3f6b886b11f92b8421f095e6d45246
Aleksander Berling <naskel@tlen.pl>
2022-11-29 17:01:20 +0100

    Removed empty space

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,8 +1,6 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
-
 Given(/^User is on the (\w+) page$/, async (page) => {
-
     await browser.url(`${browser.options.baseUrl}${page}`);
 });
 
