7b217cf8369e6afe84bc675845428d5638b010dd
Aleksander Berling <naskel@tlen.pl>
2022-11-29 05:39:00 +0100

    Fixed typo

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -3,7 +3,7 @@ const { Given, When, Then } = require('@wdio/cucumber-framework');
 
 Given(/^User is on the (\w+) page$/, async (page) => {
 
-    await browser.url(`${browser.options.baseUrl}/${page}`);
+    await browser.url(`${browser.options.baseUrl}${page}`);
 });
 
 
